5bb5bcfa82f3d20802fdb7a1d73332dfc1528315
Martín Gaitán <gaitan@gmail.com>
2025-05-26 13:50:36 -0300

    implements #13

---
+++ tisu/managers/__init__.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class TrackerManager(ABC):
+    def __init__(self, project_or_repo, auth, password):
+        self.auth = auth
+        self.password = password
+
+    @abstractmethod
+    def fetcher(self, state): ...
+
+    @abstractmethod
+    def sender(self, issues): ...
